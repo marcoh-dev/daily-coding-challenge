db0635cfab5eb95878191894218c0abb225a7c29
Marco <commits.marcoh-dev@outlook.com>
2026-07-23 22:12:20 +0200

    solved challenge

--- challenges/2026-07-23-the-reject()-function.ts
+++ challenges/2026-07-23-the-reject()-function.ts
@@ -7,11 +7,26 @@ reject([1, 2, 3, 4, 5, 6], (n) => n % 2 === 0)  =>  [1, 3, 5]
 import { test } from "@/test.ts";
 
 function reject<T>(ar: T[], predicate: (el: T) => boolean): T[] {
-  // your code here
+	return ar.filter((element) => !predicate(element));
 }
 
-test(reject([1, 2, 3, 4, 5, 6], (n) => n % 2 === 0), [1, 3, 5]);
-test(reject([1, 2, 3], (n) => n > 2), [1, 2]);
-test(reject(["a", "b", "c"], (s) => s === "b"), ["a", "c"]);
-test(reject([10, 20, 30], (n) => n < 15), [20, 30]);
-test(reject([], (n) => true), []);
+test(
+	reject([1, 2, 3, 4, 5, 6], (n) => n % 2 === 0),
+	[1, 3, 5],
+);
+test(
+	reject([1, 2, 3], (n) => n > 2),
+	[1, 2],
+);
+test(
+	reject(["a", "b", "c"], (s) => s === "b"),
+	["a", "c"],
+);
+test(
+	reject([10, 20, 30], (n) => n < 15),
+	[20, 30],
+);
+test(
+	reject([], (n) => true),
+	[],
+);
