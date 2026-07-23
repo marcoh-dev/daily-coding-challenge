bc23892c0e9c6cf75ed2ed2513dc66fce114b40a
Marco <commits.marcoh-dev@outlook.com>
2026-07-23 09:19:14 +0200

    solved warmup challenge 2026-07-23

---
+++ daily-warmup-challenges/2026-07-23.ts
@@ -0,0 +1,22 @@
+/*
+https://www.codewars.com/kata/frequency-sequence
+Return an output string that translates an input string s/$s by replacing each character in s/$s
+with a number representing the number of times that character occurs in s/$s and separating each number
+with the character(s) sep/$sep.
+*/
+
+import { test } from "@/test.ts";
+
+function freqSeq(str: string, sep: string): string {
+	const characterList = str.split("");
+	const replacedList = characterList.map(
+		(character) => characterList.filter((char) => char === character).length,
+	);
+	return replacedList.join(sep);
+}
+
+test(freqSeq("hello", "-"), "1-1-2-2-1");
+test(freqSeq("aab", ","), "2,2,1");
+test(freqSeq("abc", "|"), "1|1|1");
+test(freqSeq("aaa", "."), "3.3.3");
+test(freqSeq("abab", "-"), "2-2-2-2");
